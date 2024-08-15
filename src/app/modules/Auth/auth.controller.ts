a476ee3de01d63a4b1165995c3999002450388de
codeCampWithSifat <sayedhossainsifat300@.com>
2024-08-15 16:06:48 +0600

    some code are written in auth.controller.ts

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -8,12 +8,12 @@ const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
   const { refreshToken, ...others } = result;
 
-  const options = {
+  res.cookie('refreshToken', refreshToken, {
     secure: config.node_env === 'production',
     httpOnly: true,
-  };
-
-  res.cookie('refreshToken', refreshToken, options);
+    sameSite: 'none',
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+  });
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
