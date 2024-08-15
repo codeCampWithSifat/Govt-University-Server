a7e7c55a9677340c7ffecc2289190899a7c81830
codeCampWithSifat <sayedhossainsifat300@.com>
2024-08-15 15:47:55 +0600

    full are done

--- src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
+++ src/app/modules/EnrolledCourse/enrolledCourse.controller.ts
@@ -33,7 +33,25 @@ const updateEnrolledCourseMarks = catchAsync(async (req, res) => {
   });
 });
 
+const getMyEnrolledCourses = catchAsync(async (req, res) => {
+  const studentId = req.user.userId;
+
+  const result = await EnrolledCourseServices.getMyEnrolledCoursesFromDB(
+    studentId,
+    req.query,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Enrolled courses are retrivied succesfully',
+    meta: result.meta,
+    data: result.result,
+  });
+});
+
 export const EnrolledCourseControllers = {
   createEnrolledCourse,
   updateEnrolledCourseMarks,
+  getMyEnrolledCourses,
 };
